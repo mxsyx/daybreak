1d6c13e778396f63e56d1cf0a5e5c67b61914b3a
Mxsyx <mxsyxin@gmail.com>
2025-07-23 09:09:31 +0800

    🔨 timeline

--- src/layout/Main/Timeline.vue
+++ src/layout/Main/Timeline.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { useSceneStore } from '@/store'
+import { cn } from '@/lib/utils'
 
 const sceneStore = useSceneStore()
 const scene = computed(() => sceneStore.scene)
@@ -13,26 +14,44 @@ for (let i = 0; i < 30; i += 0.5) {
   duration += 30
   moments.value.push(i)
 }
+
+let lIndex = -1
+let rIndex = -1
 </script>
 
 <template>
   <div
     v-if="scene"
-    class="relative border-2 border-[#475569] rounded-lg h-20 max-w-max flex font-medium overflow-hidden"
+    class="relative border border-[#475569] bg-slate-900 rounded-lg h-20 max-w-max flex font-medium overflow-hidden"
   >
     <div
       v-for="(grid, index) in scene.grids"
       :key="index"
-      class="bg-[#1e293b80] cursor-pointer px-3 flex flex-col justify-center relative"
+      :class="
+        cn(
+          ' cursor-pointer px-3 flex flex-col justify-center relative hover:bg-[#1e293b80]',
+          index >= lIndex &&
+            index <= rIndex &&
+            'border-y-2 my-2 bg-[#1e293b80]',
+          index === lIndex && 'border-l-2 rounded-l-lg',
+          index === rIndex && 'border-r-2 rounded-r-lg',
+        )
+      "
       draggable="true"
     >
       <span class="whitespace-nowrap">
         {{ grid.text }}
       </span>
-      <span class="absolute top-[2px] right-1 text-xs text-muted-foreground">
+      <span
+        v-if="index < lIndex || index > rIndex"
+        class="absolute top-[2px] right-1 text-xs text-muted-foreground"
+      >
         {{ grid.start }}
       </span>
-      <div class="absolute w-px h-full right-0 bg-border"></div>
+      <div
+        v-if="lIndex === rIndex"
+        class="absolute w-px h-full right-0 bg-border"
+      ></div>
     </div>
   </div>
 </template>
